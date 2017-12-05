669539b99d049410e9f199d0626a6664a8e4221c
S.L. Nelson <sandnelson13@gmail.com>
2017-12-05 11:46:24 -0500

    Added Maps

---
+++ maps/RED.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="RED" tilewidth="16" tileheight="16" tilecount="1" columns="1">
+ <image source="../Images/RED.png" width="16" height="16"/>
+ <tile id="0">
+  <objectgroup draworder="index">
+   <object id="4" x="0.170847" y="0.341693" width="15.7179" height="15.5471"/>
+  </objectgroup>
+ </tile>
+</tileset>
